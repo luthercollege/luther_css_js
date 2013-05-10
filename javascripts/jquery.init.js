4df03f1b08da8f9831fb9f82a645d34ca0d7807d
Brian Jones <jonebr01@gmail.com>
2013-05-10 14:48:55 -0500

    css for callout class

--- javascripts/jquery.init.js
+++ javascripts/jquery.init.js
@@ -243,10 +243,10 @@ $(document).ready(function() {
 									.insertAfter($leftColumn),
 					$sections = $leftColumn
 								 .find('> div.section'),
-					targetHeight = $leftColumn
+					$targetHeight = $leftColumn
 									.height() / 2 + $sections.position().top;
 					$sections.each(function(i) {
-						if(($(this).position().top)>=targetHeight
+						if(($(this).position().top) >= $targetHeight
 						|| ($sections.length == 2 && i == 1)) {
 							$rightColumn.append($sections.slice(i));
 							return false;
